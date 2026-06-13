5ec0177ac80da3cf1e5b0eea53acc6165b6cc510
0antinomia <258392858+0antinomia@users.noreply.github.com>
2026-06-13 19:29:55 +0800

    chore: add nix dev shell

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "hlcraft.nvim development shell";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs =
+    { nixpkgs, ... }:
+    let
+      systems = [
+        "aarch64-darwin"
+        "x86_64-darwin"
+        "aarch64-linux"
+        "x86_64-linux"
+      ];
+      forAllSystems = nixpkgs.lib.genAttrs systems;
+    in
+    {
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+        {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              neovim
+              stylua
+            ];
+          };
+        }
+      );
+    };
+}
