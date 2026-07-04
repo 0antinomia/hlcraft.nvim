536848d0212983edc97b0245f5c777382de4a075
0antinomia <258392858+0antinomia@users.noreply.github.com>
2026-07-05 01:52:10 +0800

    refactor: require boolean style state

--- lua/hlcraft/ui/editor/style.lua
+++ lua/hlcraft/ui/editor/style.lua
@@ -3,17 +3,24 @@ local session = require('hlcraft.ui.session')
 local M = {}
 
 function M.next_boolean(value)
+  if value == nil then
+    return true, nil
+  end
   if value == true then
-    return false
+    return false, nil
   end
   if value == false then
-    return nil
+    return nil, nil
   end
-  return true
+  return nil, 'Style value must be boolean or nil'
 end
 
 function M.toggle(instance, result, key)
-  return session.set_style(instance, result.name, key, M.next_boolean(session.field_value(result.name, key)))
+  local next_value, err = M.next_boolean(session.field_value(result.name, key))
+  if err then
+    return false, err
+  end
+  return session.set_style(instance, result.name, key, next_value)
 end
 
 return M
